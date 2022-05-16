a8f2fa4d5f3ad6728c6acd7846cd9eeee647533a
Romain VILLARET <rovillar@e1r1p3.42.fr>
2022-05-16 17:48:15 +0200

    save

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rovillar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 16:38:27 by rovillar          #+#    #+#             */
+/*   Updated: 2022/01/18 16:38:29 by rovillar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(char *str, int c);
+char	*ft_strjoin(char *save, char *buff);
+size_t	ft_strlen(char *str);
+
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rovillar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 16:40:43 by rovillar          #+#    #+#             */
+/*   Updated: 2022/01/18 16:40:45 by rovillar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(char *str, int c);
+char	*ft_strjoin(char *save, char *buff);
+size_t	ft_strlen(char *str);
+
+#endif
